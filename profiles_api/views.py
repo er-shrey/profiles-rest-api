ee0cc859aae8b06d30eb702825904bc7b7ea36b2
Shrey Kumar Jain <in0128@fbspl.com>
2019-11-05 09:00:58 +0530

    created profile feed viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -110,3 +110,14 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     """Handle user authentication and auth tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handle creating, updating of profile feeds"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """Sets user profile to logged in user"""
+        serializer.save(user_profile=self.request.user)
